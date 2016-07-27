f60122e1bcbb53610cea12c1a2d5bef4a96a3342
hanyilou123 <hanyilou123@gmail.com>
2016-07-27 17:17:59 -0500

    [228. Summary Ranges][Accepted]committed by LeetCode Extension

--- 228-Summary-Ranges/solution.java
+++ 228-Summary-Ranges/solution.java
@@ -4,20 +4,25 @@ public class Solution {
         if(nums==null || nums.length==0)
             return res;
         String temp = ""+nums[0];
+        int count=1;
         for(int i=1; i<nums.length; i++)
         {
             if(nums[i]-nums[i-1]==1)
             {
-                start = 0;
+                if(i==nums.length-1)
+                    temp += "->"+nums[i];
+                count++;
                 continue;
             }
             else{
-                temp += "->"+nums[i-1];
+                if(count>1)
+                    temp += "->"+nums[i-1];
                 res.add(temp);
                 temp = ""+nums[i];
+                count=1;
             }
         }
-        temp+="->"+nums[nums.length-1];
+        //temp+="->"+nums[nums.length-1];
         res.add(temp);
         return res;
     }

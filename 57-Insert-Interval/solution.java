fcfe7b14333d9f631f35435700fab5d35fcf1640
hanyilou123 <hanyilou123@gmail.com>
2016-07-27 22:25:28 -0500

    [57. Insert Interval][Compile Error]committed by LeetCode Extension

---
+++ 57-Insert-Interval/solution.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+public class Solution {
+    public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
+        List<Interval> res = new ArrayList<Interval>();
+        if(intervals==null || intervals.size()==0)
+            return res;
+        if(newInterval==null)
+            return intervals;
+        int insertPos = 0;
+        for(int i=0; i<intervals.size(); i++)
+        {
+            if(intervals.get(i).end < newInterval.start)
+            {
+                res.add(intervals.get(i));
+                insertPos++;
+            }
+            else if(interval.get(i).start > newInterval.end)
+            {
+                res.add(intervals.get(i));
+            }
+            else
+            {
+                newInterval.start = Math.min(newInterval.start, intervals.get(i).start);
+                newInterval.end = Math.max(newInterval.end, intervals.get(i).end);
+            }
+        }
+        res.add(insertPos, newInterval);
+        return res;
+    }
+}

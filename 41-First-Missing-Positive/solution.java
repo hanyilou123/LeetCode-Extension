b121b1adfb571bd7f64011cd75f3fee3a245ce52
hanyilou123 <hanyilou123@gmail.com>
2016-07-28 16:11:58 -0500

    [41. First Missing Positive][Time Limit Exceeded]committed by LeetCode Extension

--- 41-First-Missing-Positive/solution.java
+++ 41-First-Missing-Positive/solution.java
@@ -5,9 +5,11 @@ public class Solution {
         int n = nums.length;
         for(int i=0; i<n; i++)
         {
-            while(nums[i]>0 && nums[i]<=n && nums[nums[i]-1]!=nums[i])
+            while(nums[i]>0 && nums[i]<=n && nums[i]!=i+1)
             {
                 int temp = nums[i];
+                if(temp==nums[nums[i]-1])
+                    break;
                 nums[i] = nums[nums[i]-1];
                 nums[nums[i]-1] = temp;
             }

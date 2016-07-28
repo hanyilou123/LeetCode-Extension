9158c903db403dec12953b43f6b045dcb18c2e29
hanyilou123 <hanyilou123@gmail.com>
2016-07-28 15:57:19 -0500

    [41. First Missing Positive][Accepted]committed by LeetCode Extension

--- 41-First-Missing-Positive/solution.java
+++ 41-First-Missing-Positive/solution.java
@@ -2,25 +2,34 @@ public class Solution {
     public int firstMissingPositive(int[] nums) {
         if(nums==null)
             return 1;
-        int n = nums.length, pos=0;
-        while(pos<n)
-        {
-            if(nums[pos]>0 && nums[pos]<=n && nums[pos]!=pos+1 && nums[nums[pos]-1]!=nums[pos])
-            {
-                int temp = nums[pos];
-                nums[pos] = nums[nums[pos]-1];
-                nums[nums[pos]-1] = temp;
-            }
-            else
-            {
-                ++pos;
-            }
+        // int n = nums.length, pos=0;
+        // while(pos<n)
+        // {
+        //     if(nums[pos]>0 && nums[pos]<=n && nums[pos]!=pos+1 && nums[nums[pos]-1]!=nums[pos])
+        //     {
+        //         int temp = nums[pos];
+        //         nums[pos] = nums[nums[pos]-1];
+        //         nums[nums[pos]-1] = temp;
+        //     }
+        //     else
+        //     {
+        //         ++pos;
+        //     }
+        // }
+        // for(int i=0; i<n; i++)
+        // {
+        //     if(nums[i]!=i+1)
+        //         return i+1;
+        // }
+        // return n+1;
+        Set<Integer> s = new HashSet<>();
+        for(int i = 0; i<nums.length; i++){
+            s.add(nums[i]);
         }
-        for(int i=0; i<n; i++)
-        {
-            if(nums[i]!=i+1)
-                return i+1;
+        int res = 1;
+        for(res = 1; res<=Integer.MAX_VALUE;res++){
+            if(!s.contains(res)) break;
         }
-        return n+1;
+        return res;
     }
 }

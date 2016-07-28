3caf98c6d4dc881347e74704e5bd78911235ab27
hanyilou123 <hanyilou123@gmail.com>
2016-07-28 16:29:16 -0500

    [41. First Missing Positive][Accepted]committed by LeetCode Extension

--- 41-First-Missing-Positive/solution.java
+++ 41-First-Missing-Positive/solution.java
@@ -2,26 +2,18 @@ public class Solution {
     public int firstMissingPositive(int[] nums) {
         if(nums==null)
             return 1;
-        // int n = nums.length;
-        // for(int i=0; i<n; i++)
-        // {
-        //     while(nums[i]>0 && nums[i]<=n && nums[i]!=(i+1))
-        //     {
-        //         int temp = nums[nums[i]-1];
-        //         if(temp==nums[i])
-        //             break;
-        //         nums[nums[i]-1] = nums[i];
-        //         nums[i] = temp;
-        //     }
-        // }
         int n = nums.length, pos=0;
         while(pos<n)
         {
             if(nums[pos]>0 && nums[pos]<=n && nums[pos]!=pos+1 && nums[nums[pos]-1]!=nums[pos])
             {
-                int temp = nums[nums[pos]-1];
+                int temp = nums[nums[pos]-1];   //必须是先保存nums[nums[pos]-1]，因为它受到了nums[pos]的影响
                 nums[nums[pos]-1] = nums[pos];
                 nums[pos] = temp;
+                
+                // temp = nums[pos]
+                // nums[pos] = nums[nums[pos]-1];
+                // nums[nums[pos]-1] = temp;
             }
             else
             {
@@ -34,23 +26,5 @@ public class Solution {
                 return i+1;
         }
         return n+1;
-        
-        
-        // int n = nums.length;
-        // for(int i = 0; i < n; ++ i){ 
-        //     while(nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]){
-        //         swap(nums, i, nums[i] - 1);
-        //     }
-        // }
-        
-        // for(int i = 0; i< n; ++ i){
-        //     if(nums[i] != i+1) return i+1;
-        // }
-        // return n+1;
     }
-    // private void swap(int[] nums,int i, int j){
-    //     int temp = nums[i];
-    //     nums[i] = nums[j];
-    //     nums[j] = temp;
-    // }
 }

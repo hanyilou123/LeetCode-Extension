7599c5356aad3b13c682e6da12b050b2e94559aa
hanyilou123 <hanyilou123@gmail.com>
2016-07-28 15:34:04 -0500

    [41. First Missing Positive][Runtime Error]committed by LeetCode Extension

--- 41-First-Missing-Positive/solution.java
+++ 41-First-Missing-Positive/solution.java
@@ -5,7 +5,7 @@ public class Solution {
         int n = nums.length, pos=0;
         while(pos<n)
         {
-            if(nums[pos]>0 && nums[pos]<=n && nums[pos]!=pos+1 && nums[nums[pos]-1]!=nums[pos])
+            if(nums[pos]!=pos+1 && nums[nums[pos]-1]!=nums[pos] && nums[pos]>0 && nums[pos]<=n)
             {
                 int temp = nums[pos];
                 nums[pos] = nums[nums[pos]-1];

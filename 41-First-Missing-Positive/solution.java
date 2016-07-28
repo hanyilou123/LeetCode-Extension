a04725014f53f3d2f21a8f7907836e8ce48699ee
hanyilou123 <hanyilou123@gmail.com>
2016-07-28 16:18:03 -0500

    [41. First Missing Positive][Accepted]committed by LeetCode Extension

--- 41-First-Missing-Positive/solution.java
+++ 41-First-Missing-Positive/solution.java
@@ -2,16 +2,30 @@ public class Solution {
     public int firstMissingPositive(int[] nums) {
         if(nums==null)
             return 1;
-        int n = nums.length;
-        for(int i=0; i<n; i++)
+        // int n = nums.length;
+        // for(int i=0; i<n; i++)
+        // {
+        //     while(nums[i]>0 && nums[i]<=n && nums[i]!=(i+1))
+        //     {
+        //         int temp = nums[nums[i]-1];
+        //         if(temp==nums[i])
+        //             break;
+        //         nums[nums[i]-1] = nums[i];
+        //         nums[i] = temp;
+        //     }
+        // }
+        int n = nums.length, pos=0;
+        while(pos<n)
         {
-            while(nums[i]>0 && nums[i]<=n && nums[i]!=(i+1))
+            if(nums[pos]>0 && nums[pos]<=n && nums[pos]!=pos+1 && nums[nums[pos]-1]!=nums[pos])
             {
-                int temp = nums[nums[i]-1];
-                if(temp==nums[i])
-                    break;
-                nums[nums[i]-1] = nums[i];
-                nums[i] = temp;
+                int temp = nums[nums[pos]-1];
+                nums[nums[pos]-1] = nums[pos];
+                nums[pos] = temp;
+            }
+            else
+            {
+                ++pos;
             }
         }
         for(int i=0; i<n; i++)

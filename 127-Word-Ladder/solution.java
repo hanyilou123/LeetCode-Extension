1b44faa17d2e36ff7eee8f77088d6d3137ed87bd
hanyilou123 <hanyilou123@gmail.com>
2016-07-28 18:16:28 -0500

    [127. Word Ladder][Compile Error]committed by LeetCode Extension

---
+++ 127-Word-Ladder/solution.java
@@ -0,0 +1,56 @@
+public class Solution {
+    public int ladderLength(String beginWord, String endWord, Set<String> wordList) {
+        if(wordList==null)
+            return 0;
+        if(beginWord.equals(endWord))
+            return 1;
+        wordList.add(beginWord);
+        wordList.add(endWord);
+        Queue<String> queue = new LinkedList<String>();
+        Set<String> hash = new HashSet<String>();
+        queue.offer(beginWord);
+        hash.add(beginWord);
+        int length = 1;
+        while(!queue.empty())
+        {
+            length++;
+            for(int i=0; i<queue.size(); i++)
+            {
+                String word = queue.poll();
+                for(String nextword: getNextWord(word, wordList))
+                {
+                    if(hash.contains(nextword))
+                        continue;
+                    if(nextword.equals(endWord))
+                        return length;
+                    queue.offer(nextword);
+                    hash.add(nextword);
+                }
+                
+            }
+        }
+        return 0;
+    }
+    public ArrayList<String> getNextWord(String word, Set<String> wordList)
+    {
+        ArrayList<String> nextWordList = new ArrayList<String>();
+        for(int i=0; i<word.length(); i++)
+        {
+            for(char c='a'; c<='z'; c++)
+            {
+                if(word.charAt(i)==c)
+                    continue;
+                String nextword = replaceChar(word, i, c);
+                if(wordList.contains(nextword))
+                    nextWordList.add(nextword);
+            }
+        }
+        return nextWordList;
+    }
+    public String replaceChar(String word, int index, char c)
+    {
+        char[] newString = word.toCharArray();
+        newString[index] = c;
+        return new String(newString);
+    }
+}

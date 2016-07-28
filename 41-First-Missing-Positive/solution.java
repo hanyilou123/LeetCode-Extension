e9d4465ff01b09e3f91a44c26639d9a8b33520bc
hanyilou123 <hanyilou123@gmail.com>
2016-07-28 16:02:21 -0500

    [41. First Missing Positive][Compile Error]committed by LeetCode Extension

--- 41-First-Missing-Positive/solution.java
+++ 41-First-Missing-Positive/solution.java
@@ -2,38 +2,38 @@ public class Solution {
     public int firstMissingPositive(int[] nums) {
         if(nums==null)
             return 1;
-        int n = nums.length, pos=0;
-        while(pos<n)
-        {
-            if(nums[pos]>0 && nums[pos]<=n &&  nums[nums[pos]-1]!=nums[pos])
-            {
-                int temp = nums[pos];
-                nums[pos] = nums[nums[pos]-1];
-                nums[nums[pos]-1] = temp;
-            }
-            else
-            {
-                ++pos;
-            }
-        }
-        for(int i=0; i<n; i++)
-        {
-            if(nums[i]!=i+1)
-                return i+1;
-        }
-        return n+1;
-        
-        
-        // int n = nums.length;
-        // for(int i = 0; i < n; ++ i){ 
-        //     while(nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]){
-        //         swap(nums, i, nums[i] - 1);
+        // int n = nums.length, pos=0;
+        // while(pos<n)
+        // {
+        //     if(nums[pos]>0 && nums[pos]<=n && nums[pos]!=pos+1 && nums[nums[pos]-1]!=nums[pos])
+        //     {
+        //         int temp = nums[pos];
+        //         nums[pos] = nums[nums[pos]-1];
+        //         nums[nums[pos]-1] = temp;
+        //     }
+        //     else
+        //     {
+        //         ++pos;
         //     }
         // }
-        
-        // for(int i = 0; i< n; ++ i){
-        //     if(nums[i] != i+1) return i+1;
+        // for(int i=0; i<n; i++)
+        // {
+        //     if(nums[i]!=i+1)
+        //         return i+1;
         // }
         // return n+1;
+        
+        
+        int n = nums.length;
+        for(int i = 0; i < n; ++ i){ 
+            while(nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]){
+                swap(nums, i, nums[i] - 1);
+            }
+        }
+        
+        for(int i = 0; i< n; ++ i){
+            if(nums[i] != i+1) return i+1;
+        }
+        return n+1;
     }
 }

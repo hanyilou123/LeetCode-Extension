80d0c216271d87bac530bb6be2ae0c1b54c65b77
hanyilou123 <hanyilou123@gmail.com>
2016-07-28 22:27:48 -0500

    [205. Isomorphic Strings][Compile Error]committed by LeetCode Extension

---
+++ 205-Isomorphic-Strings/solution.java
@@ -0,0 +1,28 @@
+public class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if(s==null || t==null)
+            return false;
+        if(s.length()!=t.length())
+            return false;
+        Map<Charater, Charater> map = HashMap<Charater, Charater>();
+        Set<Charater> set = new HashSet<Charater>();
+        for(int i=0; i<s.length(); i++)
+        {
+            char sc = s.charAt(i);
+            char tc = t.charAt(i);
+            if(map.containsKey(sc))
+            {
+                if(map.get(sc)!=tc)
+                    return false;
+            }
+            else
+            {
+                if(set.contains(tc)) return false;
+                else{
+                    map.put(sc, tc);
+                    set.add(tc);
+                }
+            }
+        }
+    }
+}

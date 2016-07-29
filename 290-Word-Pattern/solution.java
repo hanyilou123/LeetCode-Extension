b14c0e3f8d941b069a70773bab4a9204e9225e82
hanyilou123 <hanyilou123@gmail.com>
2016-07-29 15:47:35 -0500

    [290. Word Pattern][Accepted]committed by LeetCode Extension

--- 290-Word-Pattern/solution.java
+++ 290-Word-Pattern/solution.java
@@ -4,7 +4,10 @@ public class Solution {
             return false;
         String[] splited = str.split(" ");
         char[] pat = pattern.toCharArray();
+        if(pat.length!=splited.length)
+            return false;
         Map<Character, String> hash = new HashMap<Character, String>();
+        Set<String> set = new HashSet<String>();
         for(int i=0; i<pat.length; i++)
         {
             if(hash.containsKey(pat[i]))
@@ -13,7 +16,10 @@ public class Solution {
                     return false;
             }
             else{
+                if(set.contains(splited[i]))
+                    return false;
                 hash.put(pat[i], splited[i]);
+                set.add(splited[i]);
             }
         }
         return true;

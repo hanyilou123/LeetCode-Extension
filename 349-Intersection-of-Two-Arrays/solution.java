d4c4f4a2d50a0954726ad3b1f4cfff119097227b
hanyilou123 <hanyilou123@gmail.com>
2016-07-28 23:36:54 -0500

    [349. Intersection of Two Arrays][Compile Error]committed by LeetCode Extension

--- 349-Intersection-of-Two-Arrays/solution.java
+++ 349-Intersection-of-Two-Arrays/solution.java
@@ -4,23 +4,21 @@ public class Solution {
             return null;
         Set<Integer> res1 = new HashSet<Integer>();
         Set<Integer> res2 = new HashSet<Integer>();
-        Set<Integer> res3 = new HashSet<Integer>();
+        
         for(int i=0; i<nums1.length; i++)
             res1.add(nums1[i]);
         for(int i=0; i<nums2.length; i++)
-            res2.add(nums2[i]);
-        for(int i=0; i<res1.size(); i++)
         {
-            if(res2.contains(res1.get(i)))
-                res3.add(res1.get(i));
+            if(res1.contains(nums2[i]) && !res2.contains(nums[i]))
+                res2.add(nums2[i]);
         }
-        if(res3)
+        
+        int size = res2.size();
+        int[] result = new int[size];
+        int index = 0;
+        for(int each : res2)
         {
-            int[] result = new int[res3.size()];
-            for(int i=0; i<res3.size(); i++)
-            {
-                result[i] = res3.get(i);
-            }
+            result[index++] = each;
         }
         return result;
     }

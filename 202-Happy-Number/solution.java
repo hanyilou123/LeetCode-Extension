8c2b2e3e0c59bfb2294e9a454d4903ad536a16f1
hanyilou123 <hanyilou123@gmail.com>
2016-07-29 13:07:47 -0500

    [202. Happy Number][Compile Error]committed by LeetCode Extension

---
+++ 202-Happy-Number/solution.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> hash = new HashSet<Integer>();
+        while(n!=1)
+        {
+            if(hash.contains(n))
+                return false;
+            hash.add(n);
+            n = getNext(n);
+        }
+    }
+    public int getNext(int num)
+    {
+        int total = 0;
+        while(num!=0)
+        {
+            int h = num%10;
+            total += h*h;
+            num /= 10;
+        }
+        return total;
+    }
+}

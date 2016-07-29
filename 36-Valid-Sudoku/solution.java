cb01d10a60ad7ee0aa16a5bd55452300c7ccf43d
hanyilou123 <hanyilou123@gmail.com>
2016-07-28 23:03:36 -0500

    [36. Valid Sudoku][Compile Error]committed by LeetCode Extension

---
+++ 36-Valid-Sudoku/solution.java
@@ -0,0 +1,44 @@
+public class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        boolean[] visited = new boolean[9];
+        for(int i=0; i<9; i++)
+        {
+            Arrays.fill(visited, false);
+            for(int j=0; j<9; j++)
+            {
+                if(!visitProcess(board[i][j], visited))
+                    return false;
+            }
+        }
+        for(int i=0; i<9; i++)
+        {
+            Arrays.fill(visited, false);
+            for(int j=0; j<9; j++)
+            {
+                if(!visitProcess(board[j][i], visited))
+                    return false;
+            }
+        }
+        for(int i=0; i<9; i+=3)
+        {
+            for(int j=0; j<9; j+=3)
+            {
+                for(int k=0; k<9; k++)
+                {
+                    if(!visitProcess(board[i+k/3][j+k%3]))
+                        return false;
+                }
+            }
+        }
+        return true;
+    }
+    public visitProcess(char c, boolean[] visited)
+    {
+        if(c=='.')
+            return true;
+        int num = c-'0';
+        if(num<1 || num>9 || visited[num-1]==true)
+            return false;
+        visited[nums-1] = true;
+    }
+}

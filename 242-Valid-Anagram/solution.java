812c5110a6ab608c7b2f9af5df068cea69468313
hanyilou123 <hanyilou123@gmail.com>
2016-07-29 12:53:37 -0500

    [242. Valid Anagram][Accepted]committed by LeetCode Extension

--- 242-Valid-Anagram/solution.java
+++ 242-Valid-Anagram/solution.java
@@ -2,34 +2,20 @@ public class Solution {
     public boolean isAnagram(String s, String t) {
         if(s==null || t==null)
             return false;
-        Map<Character, Integer> res1 = new HashMap<Character, Integer>();
-        Map<Character, Integer> res2 = new HashMap<Character, Integer>();
+        int[] model = new int[26];
         for(int i=0; i<s.length(); i++)
         {
             char c1 = s.charAt(i);
-            if(res1.containsKey(c1))
-            {
-                int num = res1.get(c1);
-                res1.put(c1, ++num);
-            }
-            else
-                res1.put(c1, 1);
+            model[c1-'a']++;
         }
         for(int i=0; i<t.length(); i++)
         {
             char c2 = t.charAt(i);
-            if(res1.containsKey(c2))
-            {
-                int num = res1.get(c2);
-                res1.put(c2, --num);
-            }
-            else{
-                return false;
-            }
+            model[c2-'a']--;
         }
-        for(Map.Entry<Character, Integer> entry: res1.entrySet())
+        for(int i=0; i<26; i++)
         {
-            if(entry.getValue()!=0)
+            if(model[i]!=0)
                 return false;
         }
         return true;

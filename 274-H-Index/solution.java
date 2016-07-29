45d4fb506e3dda78e6acb424656f74f7dc73bcb2
hanyilou123 <hanyilou123@gmail.com>
2016-07-29 18:39:28 -0500

    [274. H-Index][Runtime Error]committed by LeetCode Extension

--- 274-H-Index/solution.java
+++ 274-H-Index/solution.java
@@ -5,7 +5,7 @@ public class Solution {
         int hIndex = 0;
         for(int i=len-1; i>=0; i++)
         {
-            if(len-i>citations[i])
+            if(len-i>=citations[i])
             {
                 return len-i;
             }

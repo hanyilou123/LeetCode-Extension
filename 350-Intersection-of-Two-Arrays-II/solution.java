3cce80fc546571e9e47d084571a1ceb08fd6b96b
hanyilou123 <hanyilou123@gmail.com>
2016-07-29 12:17:42 -0500

    [350. Intersection of Two Arrays II][Compile Error]committed by LeetCode Extension

--- 350-Intersection-of-Two-Arrays-II/solution.java
+++ 350-Intersection-of-Two-Arrays-II/solution.java
@@ -2,8 +2,8 @@ public class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
         if(nums1==null || nums2==null)
             return null;
-        Map<Integer, Integer> res1 = new Map<Integer, Integer>();
-        Map<Integer, Integer> res2 = new Map<Integer, Integer>();
+        Map<Integer, Integer> res1 = new HashMap<Integer, Integer>();
+        Map<Integer, Integer> res2 = new HashMap<Integer, Integer>();
         for(int i=0; i<nums1.length; i++)
         {
             if(res1.containsKey(nums1[i]))

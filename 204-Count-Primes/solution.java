e0c55389dafbdfb3a63a65276576f1727a035cb5
hanyilou123 <hanyilou123@gmail.com>
2016-07-28 20:52:09 -0500

    [204. Count Primes][Time Limit Exceeded]committed by LeetCode Extension

---
+++ 204-Count-Primes/solution.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public int countPrimes(int n) {
+        if(n<=1)
+            return 0;
+        int count=0;
+        for(int i=2; i<n; i++)
+        {
+            if(isPrime(i))
+                count++;
+        }
+        return count;
+    }
+    public boolean isPrime(int num)
+    {
+        for(int i=2; i*i<=num; i++)
+        {
+            if(num%i==0)
+                return false;
+        }
+        return true;
+    }
+}

59cbfe1603d407699b68e005819e0d24e562c6f6
hanyilou123 <hanyilou123@gmail.com>
2016-07-29 19:00:38 -0500

    [274. H-Index][Accepted]committed by LeetCode Extension

--- 274-H-Index/solution.java
+++ 274-H-Index/solution.java
@@ -3,14 +3,14 @@ public class Solution {
         Arrays.sort(citations);
         int len = citations.length;
         int[] newci = new int[len];
-        for(int i=len-1; i>=0; i++)
+        for(int i=len-1; i>=0; i--)
         {
             newci[len-1-i] = citations[i];
         }
         int hIndex = 0;
         for(int i=0; i<len; i++)
         {
-            if(i>=citations[i])
+            if(i>=newci[i])
             {
                 return i;
             }

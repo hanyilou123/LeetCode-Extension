f5ef854c2c2dc4402e3a5af8563a76fbb6f21f23
hanyilou123 <hanyilou123@gmail.com>
2016-07-30 16:10:06 -0500

    [347. Top K Frequent Elements][Compile Error]committed by LeetCode Extension

---
+++ 347-Top-K-Frequent-Elements/solution.java
@@ -0,0 +1,43 @@
+public class Solution {
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        List<Integer> res = new ArrayList<Integer>();
+        Map<Integer, Integer> hash = HashMap<Integer, Integer>();
+        PriorityQueue<Map.Entry<Integer,Integer>> que = new PriorityQueue<Map.Entry<Integer,Integer>>(
+            new Comparator<Map.Entry<Integer, Integer>>(){
+                public int compare(Map.Entry<Integer,Integer>e1, Map.Entry<Integer, Integer>e2)
+                {
+                    return e1.getValue() - e2.getValue();
+                }
+            }
+        );
+        for(int i=0; i<nums.length; i++)
+        {
+            if(hash.containsKey(nums[i]))
+            {
+                int t = hash.get(nums[i]);
+                hash.put(nums[i], t+1);
+            }
+            else
+            {
+                hash.put(nums[i], 1);
+            }
+        }
+        for(Map.Entry<Integer, Integer> entry: hash.entrySet())
+        {
+            if(que.size()<k)
+            {
+                que.offer(entry);
+            }
+            else if(que.peek().getValue()<entry.getValue())
+            {
+                que.poll();
+                que.offer(entry);
+            }
+        }
+        for(Map.Entry<Integer, Integer> entry: que.entrySet())
+        {
+            res.add(entry.getKey());
+        }
+        return res;
+    }
+}

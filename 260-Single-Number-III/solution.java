ed1b796492952f3ea7a8c2d1f7e52edd1292e35b
hanyilou123 <hanyilou123@gmail.com>
2016-07-31 00:19:39 -0500

    [260. Single Number III][Compile Error]committed by LeetCode Extension

--- 260-Single-Number-III/solution.java
+++ 260-Single-Number-III/solution.java
@@ -1,7 +1,5 @@
 public class Solution {
     public int[] singleNumber(int[] nums) {
-        if(nums==null || nums.length==0)
-            return -1;
         int xor = 0;
         for(int i=0; i<nums.length; i++)
         {

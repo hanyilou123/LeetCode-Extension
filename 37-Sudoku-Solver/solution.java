a1c1cb37ab198d47af7c45abce8e03267e8852e4
hanyilou123 <hanyilou123@gmail.com>
2016-07-31 16:46:17 -0500

    [37. Sudoku Solver][Compile Error]committed by LeetCode Extension

---
+++ 37-Sudoku-Solver/solution.java
@@ -0,0 +1,63 @@
+public class Solution {
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+    public solve(char[][] board)
+    {
+        int h=board.length, w=board[0].length;
+        for(int i=0; i<h; i++)
+        {
+            for(int j=0; j<w; j++)
+            {
+                if(board[i][j]!='.')
+                    continue;
+                for(int k=1; k<=9; k++)
+                {
+                    board[i][j] = '0'+k;
+                    if(isValid(board, i, j) && solve(board))
+                    {
+                        return true;
+                    }
+                    board[i][j]='.';
+                }
+                return false;
+            }
+        }
+        return true;
+    }
+    public boolean isValid(char[][] board, int h, int w)
+    {
+        Set<Character> ct = new HashSet<Character>();
+        for(int j=0; j<9; j++)
+        {
+            if(ct.contains(board[h][j]))
+                return false;
+            if(board[h][j]>='1' && board[h][j]<='9')
+                ct.add(board[h][j]);
+        }
+        
+        ct.clear();
+        for(int i=0; i<9; i++)
+        {
+            if(ct.contains(board[i][w]))
+                return false;
+            if(board[i][w]>='1' && board[i][w]<='9')
+                ct.add(board[i][w]);
+        }
+        
+        ct.clear();
+        for(int i=0; i<3; i++)
+        {
+            for(int j=0; j<3; j++)
+            {
+                int m = h/3*3 + i;
+                int n = w/3*3 + j;
+                if(ct.contains(board[m][n]))
+                    return false;
+                if(board[m][n]>='1' && board[m][n]<='9')
+                    ct.add(board[m][n]);
+            }
+        }
+        return true;
+    }
+}

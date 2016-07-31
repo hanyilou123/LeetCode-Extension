72e566594b3f1969500f9e7ac921355206b77ec9
hanyilou123 <hanyilou123@gmail.com>
2016-07-30 23:49:59 -0500

    [137. Single Number II][Compile Error]committed by LeetCode Extension

--- 137-Single-Number-II/solution.java
+++ 137-Single-Number-II/solution.java
@@ -2,7 +2,7 @@ public class Solution {
     public int singleNumber(int[] nums) {
         if(nums==null || nums.length==0)
             return -1;
-        int[] bitnum = new int[32];
+        int[] bitnums = new int[32];
         int res = 0;
         for(int i=0; i<32; i++)
         {

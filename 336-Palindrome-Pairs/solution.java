eb3969b6a53e7fd903a3b228a5fe0e6e660306cf
hanyilou123 <hanyilou123@gmail.com>
2016-07-31 20:58:01 -0500

    [336. Palindrome Pairs][Compile Error]committed by LeetCode Extension

---
+++ 336-Palindrome-Pairs/solution.java
@@ -0,0 +1,34 @@
+public class Solution {
+    public List<List<Integer>> palindromePairs(String[] words) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if(words==null || words.length)
+            return result;
+        for(int i=0; i<words.length; i++)
+        {
+            for(int j=0; j<words.length; j++)
+            {
+                if(i!=j)
+                {
+                    String temp = ""+words[i]+word[j];
+                    List<Integer> group = ArrayList<Integer>();
+                    if(isPalindrome(temp))
+                    {
+                        group.add(i);
+                        group.add(j);
+                        result.add(group);
+                    }
+                }
+            }
+        }
+    }
+    public boolean isPalindrome(String str)
+    {
+        int len = str.length();
+        for(int i=0; i<len/2; i++)
+        {
+            if(str.charAt(i)!=str.charAt(len-i-1))
+                return false;
+        }
+        return true;
+    }
+}

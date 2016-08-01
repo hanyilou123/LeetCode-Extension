733acaff6f889dc83ca8843ea909fc1bfcf44b91
hanyilou123 <hanyilou123@gmail.com>
2016-08-01 15:03:41 -0500

    [234. Palindrome Linked List][Compile Error]committed by LeetCode Extension

---
+++ 234-Palindrome-Linked-List/solution.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if(head==null || head.next==null)
+            return true;
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast!=null && fast.next!=null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        if(fast) //奇数
+        {
+            slow = reverse(slow.next);
+        }
+        else   //偶数
+        {
+            slow = reverse(slow);
+        }
+    }
+    public ListNode reverse(ListNode head)
+    {
+        ListNode pre = null;
+        if(head!=null)
+        {
+            ListNode temp = head.next;
+            head.next = pre;
+            pre = head;
+            head = temp
+        }
+    }
+}

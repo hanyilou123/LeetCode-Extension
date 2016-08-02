adb0baf302ddff3c7dc37ada2e6736aa30e152e8
hanyilou123 <hanyilou123@gmail.com>
2016-08-02 17:44:21 -0500

    [43. Multiply Strings][Runtime Error]committed by LeetCode Extension

--- 43-Multiply-Strings/solution.java
+++ 43-Multiply-Strings/solution.java
@@ -4,11 +4,11 @@ public class Solution {
             return null;
         int len1=num1.length(), len2=num2.length(), len3=len1+len2;
         int[] num3 = new int[len3];
-        int carry=0, product=0;
-        for(int i=len1-1; i>=0; i--)
+        int carry=0, product=0, i=0, j=0;
+        for(i=len1-1; i>=0; i--)
         {
             carry=0;
-            for(int j=len2-1; j>=0; j--)
+            for(j=len2-1; j>=0; j--)
             {
                 product = (num1.charAt(i)-'0')*(num2.charAt(j)-'0') + carry + num3[i+j+1];
                 num3[i+j+1] = product%10;

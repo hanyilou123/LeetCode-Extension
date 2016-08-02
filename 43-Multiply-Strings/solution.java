9ec303d690e6664e52b45daf9200aa99ca3cec3d
hanyilou123 <hanyilou123@gmail.com>
2016-08-02 17:43:25 -0500

    [43. Multiply Strings][Compile Error]committed by LeetCode Extension

---
+++ 43-Multiply-Strings/solution.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public String multiply(String num1, String num2) {
+        if(num1==null || num2==null)
+            return null;
+        int len1=num1.length(), len2=num2.length(), len3=len1+len2;
+        int[] num3 = new int[len3];
+        int carry=0, product=0;
+        for(int i=len1-1; i>=0; i--)
+        {
+            carry=0;
+            for(int j=len2-1; j>=0; j--)
+            {
+                product = (num1.charAt(i)-'0')*(num2.charAt(j)-'0') + carry + num3[i+j+1];
+                num3[i+j+1] = product%10;
+                carry = product/10;
+            }
+            num3[i+j+1]=carry;
+        }
+        StringBuilder sb = new StringBuilder();
+        int index=0;
+        while(num3[index]==0 && index<len3)
+            index++;
+        while(index<len3)
+        {
+            sb.append(num3[index++]);
+        }
+        return sb.toString();
+    }
+}

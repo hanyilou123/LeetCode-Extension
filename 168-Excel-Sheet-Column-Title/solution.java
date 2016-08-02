1286f1af2db6947bafd54165751d685dda9450d3
hanyilou123 <hanyilou123@gmail.com>
2016-08-02 00:23:20 -0500

    [168. Excel Sheet Column Title][Runtime Error]committed by LeetCode Extension

---
+++ 168-Excel-Sheet-Column-Title/solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public String convertToTitle(int n) {
+        String res = "";
+        char[] alpha = new char[26];
+        for(int i=0; i<26; i++)
+            alpha[i] = (char)('A'+i);
+        int i=0;
+        while(i<(n/27))
+        {
+            res+="A";
+        }
+        res += alpha[n%27];
+        return res;
+    }
+}

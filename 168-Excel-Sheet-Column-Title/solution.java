8aeb384388854b6f60b8ed6f347b0d15aa821a99
hanyilou123 <hanyilou123@gmail.com>
2016-08-02 15:17:52 -0500

    [168. Excel Sheet Column Title][Accepted]committed by LeetCode Extension

--- 168-Excel-Sheet-Column-Title/solution.java
+++ 168-Excel-Sheet-Column-Title/solution.java
@@ -1,21 +1,11 @@
 public class Solution {
     public String convertToTitle(int n) {
         String res = "";
-        char[] alpha = new char[26];
-        for(int i=0; i<26; i++)
-            alpha[i] = (char)('A'+i);
-        int i=0;
-        if(n>26)
+        while(n>0)
         {
-            while(i<(n/27))
-            {
-                res+="A";
-                i++;
-            }
-            res += alpha[n%26-1];
+            res = (char)((n-1)%26+'A') + res;
+            n = (n-1)/26;
         }
-        else
-            res += alpha[n-1];
         return res;
     }
 }

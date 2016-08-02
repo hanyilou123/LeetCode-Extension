328073739cb9b96a0426bccea52c1c20e06200ce
hanyilou123 <hanyilou123@gmail.com>
2016-08-01 21:22:52 -0500

    [264. Ugly Number II][Compile Error]committed by LeetCode Extension

--- 264-Ugly-Number-II/solution.java
+++ 264-Ugly-Number-II/solution.java
@@ -2,16 +2,16 @@ public class Solution {
     public int nthUglyNumber(int n) {
         if(n==1) return 1;
         PriorityQueue<Integer> q = new PriorityQueue<Integer>();
-        q.add(1l);
+        q.offer(1l);
         int temp=0;
         for(int i=1; i<n; i++)
         {
             temp = q.poll();
             while(!q.isEmpty() && q.peek()==temp)
                 temp = q.poll();
-            q.add(temp*2);
-            q.add(temp*3);
-            q.add(temp*5);
+            q.offer(temp*2);
+            q.offer(temp*3);
+            q.offer(temp*5);
         }
         return temp;
     }

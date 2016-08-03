7e474e7b29fa14217b65606ca73c8649a077d8bb
hanyilou123 <hanyilou123@gmail.com>
2016-08-02 23:23:48 -0500

    [343. Integer Break][Compile Error]committed by LeetCode Extension

---
+++ 343-Integer-Break/solution.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public int integerBreak(int n) {
+        if(n<4)
+            return n-1;
+        int res=1;
+        while(n>3)
+        {
+            res *=3;
+            n -=3;
+        }
+        if(n==0)
+            return res;
+        if(n==1)
+            return res/3*4;
+        if(n==2)
+            return res*2;
+    }
+}

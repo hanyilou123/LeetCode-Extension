923a36f6fad898883f476c60f31088070d80bc3b
hanyilou123 <hanyilou123@gmail.com>
2016-08-02 20:38:37 -0500

    [357. Count Numbers with Unique Digits][Compile Error]committed by LeetCode Extension

---
+++ 357-Count-Numbers-with-Unique-Digits/solution.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public int countNumbersWithUniqueDigits(int n) {
+        if(n>=10)
+            n=10;
+        if(n=0)
+            return 1;
+        int result=0;
+        for(int i=1; i<=n; i++)
+        {
+            int bigrow=9;
+            if(i==1)
+                bigrow=10;
+            else
+            {
+                int j=9;
+                while(i+j>10)
+                {
+                    bigrow *= j;
+                    j--;
+                }
+            }
+            result += bigrow;
+        }
+        return result;
+    }
+}

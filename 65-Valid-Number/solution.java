7a5f1f8a29922da43885021c703f6514d2a56a7d
hanyilou123 <hanyilou123@gmail.com>
2016-08-03 18:06:04 -0500

    [65. Valid Number][Compile Error]committed by LeetCode Extension

---
+++ 65-Valid-Number/solution.java
@@ -0,0 +1,40 @@
+public class Solution {
+    public boolean isNumber(String s) {
+        boolean numSeen=false, eSeen=false, pointSeen=false, numAfterE=true;
+        s.trim();
+        for(int i=0; i<s.length(); i++)
+        {
+            char c=s.charAt(i);
+            if(c>='0' && c<='9')
+            {
+                numSeen=true;
+                numAfterE=true;
+            }
+            else if(c=='.')
+            {
+                if(eSeen || pointSeen)
+                {
+                    return false;
+                }
+                pointSeen=true;
+            }
+            else if(c=='e')
+            {
+                if(eSeen || !numSeen)
+                {
+                    return false;
+                }
+                eSeen=true;
+                numAfterE=false;
+            }
+            else if(c=='+' || c=="-")
+            {
+                if(i!=0 && s.charAt(i-1)!='e')
+                    return false;
+            }
+            else 
+                return false;
+        }
+        return numSeen&&numAfterE;
+    }
+}

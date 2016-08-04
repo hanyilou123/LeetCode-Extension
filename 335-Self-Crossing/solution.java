2700e934c5deb83b46bdfe05866f3a45a26df975
hanyilou123 <hanyilou123@gmail.com>
2016-08-04 14:04:38 -0500

    [335. Self Crossing][Compile Error]committed by LeetCode Extension

---
+++ 335-Self-Crossing/solution.java
@@ -0,0 +1,23 @@
+public class Solution {
+    // http://www.cnblogs.com/grandyang/p/5216856.html
+    public boolean isSelfCrossing(int[] x) {
+        if(x.length<=3)
+            return false;
+        for(int i=3; i<x.length; i++)
+        {
+            if(x[i]>=x[i-2] && x[i-1]<=x[i-3])
+                return true;
+            if(x>=4)
+            {
+                if(x[i-1]==x[i-3] && x[i]+x[i-4]>=x[i-2])
+                    return true;
+            }
+            if(x>=5)
+            {
+                if(x[i]+x[i-4]>=x[i-2] && x[i-1]+x[i-5]>=x[i-3] && x[i-2]>=x[i-4] && x[i-1]<=x[i-3])
+                    return true;
+            }
+        }
+        return false;
+    }
+}

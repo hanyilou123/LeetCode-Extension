9a80d6230a7abb7d4d743356fbcecae03b0bfa3e
hanyilou123 <hanyilou123@gmail.com>
2016-08-04 14:05:27 -0500

    [335. Self Crossing][Accepted]committed by LeetCode Extension

--- 335-Self-Crossing/solution.java
+++ 335-Self-Crossing/solution.java
@@ -7,12 +7,12 @@ public class Solution {
         {
             if(x[i]>=x[i-2] && x[i-1]<=x[i-3])
                 return true;
-            if(x>=4)
+            if(i>=4)
             {
                 if(x[i-1]==x[i-3] && x[i]+x[i-4]>=x[i-2])
                     return true;
             }
-            if(x>=5)
+            if(i>=5)
             {
                 if(x[i]+x[i-4]>=x[i-2] && x[i-1]+x[i-5]>=x[i-3] && x[i-2]>=x[i-4] && x[i-1]<=x[i-3])
                     return true;

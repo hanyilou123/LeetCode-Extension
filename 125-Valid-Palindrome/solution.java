e2890be37887e4ab9c4bd25f8f93332d238e3904
hanyilou123 <hanyilou123@gmail.com>
2016-08-04 14:31:19 -0500

    [125. Valid Palindrome][Time Limit Exceeded]committed by LeetCode Extension

--- 125-Valid-Palindrome/solution.java
+++ 125-Valid-Palindrome/solution.java
@@ -5,9 +5,9 @@ public class Solution {
         int i=0, j=s.length()-1;
         while(i<j)
         {
-            while(i<s.length() && (s.charAt(i)<'A' || (s.charAt(i)>'Z' && s.charAt(i)<'a') || s.charAt(i)>'z'))
+            while(i<s.length() && i<j && (s.charAt(i)<'A' || (s.charAt(i)>'Z' && s.charAt(i)<'a') || s.charAt(i)>'z'))
                 i++;
-            while(j>=0 && (s.charAt(j)<'A' || (s.charAt(j)>'Z' && s.charAt(j)<'a') || s.charAt(j)>'z'))
+            while(j>=0 && i<j && (s.charAt(j)<'A' || (s.charAt(j)>'Z' && s.charAt(j)<'a') || s.charAt(j)>'z'))
                 j--;
             if(s.charAt(i)==s.charAt(j) || s.charAt(i)==(char)(s.charAt(j)+32) || s.charAt(i)==(char)(s.charAt(j)-32))
                 continue;

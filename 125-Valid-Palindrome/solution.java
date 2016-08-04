620fde7b5b012917831a06b02b86b4e1cb67c147
hanyilou123 <hanyilou123@gmail.com>
2016-08-04 14:24:48 -0500

    [125. Valid Palindrome][Runtime Error]committed by LeetCode Extension

---
+++ 125-Valid-Palindrome/solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public boolean isPalindrome(String s) {
+        if(s==null || s.length()==0 || s.length()==1)
+            return true;
+        int i=0, j=s.length()-1;
+        while(i<j)
+        {
+            while(i<s.length() && (s.charAt(i)<'A' || (s.charAt(i)>'Z' && s.charAt(i)<'a') || s.charAt(i)>'z'))
+                i++;
+            while(j>=0 && (s.charAt(j)<'A' || (s.charAt(j)>'Z' && s.charAt(j)<'a') || s.charAt(j)>'z'))
+                j--;
+            if(s.charAt(i)==s.charAt(j) || s.charAt(i)==(char)(s.charAt(j)+32) || s.charAt(i)==(char)(s.charAt(j)-32))
+                continue;
+            else
+                return false;
+        }
+        return true;
+    }
+}

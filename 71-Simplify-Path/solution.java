7ed097da8defd1ccb38bc9960d151845c07b3a12
hanyilou123 <hanyilou123@gmail.com>
2016-08-04 23:23:59 -0500

    [71. Simplify Path][Accepted]committed by LeetCode Extension

---
+++ 71-Simplify-Path/solution.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public String simplifyPath(String path) {
+        String result = "/";
+        String[] splitStr = path.split("/");
+        List<String> temp = new LinkedList<String>();
+        
+        for(String s: splitStr)
+        {
+            if(s.equals(".."))
+            {
+                if(temp.size()>0)
+                {
+                    temp.remove(temp.size()-1);
+                }
+            }
+            else if(!s.equals(".") && !s.equals(""))
+            {
+                temp.add(s);
+            }
+        }
+        for(String s:temp)
+        {
+            result += s + "/";
+        }
+        if(result.length()>1)
+            return result.substring(0, result.length()-1);
+        return result;
+    }
+}

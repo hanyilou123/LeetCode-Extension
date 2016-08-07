ef0c238b3e3f3a02eff937ac167ac9ae1a043ef8
hanyilou123 <hanyilou123@gmail.com>
2016-08-07 15:29:27 -0500

    [215. Kth Largest Element in an Array][Compile Error]committed by LeetCode Extension

--- 215-Kth-Largest-Element-in-an-Array/solution.java
+++ 215-Kth-Largest-Element-in-an-Array/solution.java
@@ -1,6 +1,44 @@
 public class Solution {
     public int findKthLargest(int[] nums, int k) {
-        Arrays.sort(nums);
-        return nums[nums.length-k];
+        if(nums==null || nums.length==0 || k<=0)
+            return 0;
+        return helper();
+    }
+    public int helper(int[] nums, int l, int r, int k)
+    {
+        if(l==r)
+            return nums[l];
+        int pos = patition(nums, l, r);
+        if(pos+1==k)
+        {
+            return nums[pos];
+        }
+        else if(pos+1<k)
+        {
+            return helper(nums, pos+1, right, k);
+        }
+        else
+        {
+            return helper(nums, l, pos-1, k);
+        }
+    }
+    public int patition(int[] nums, int l, int r)
+    {
+        int left=l, right=r, pivot = nums[l];
+        while(left<right)
+        {
+            while(left<right && nums[right]>=pivot)
+            {
+                right--;
+            }
+            nums[left]=nums[right];
+            while(left<right && nums[left]<=pivot)
+            {
+                left++;
+            }
+            nums[right]=nums[left];
+        }
+        nums[left]=pivot;
+        return left;
     }
 }

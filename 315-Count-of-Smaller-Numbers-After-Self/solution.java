a03ee0f943156568d3f2fc2a5aa8903842efa2c7
hanyilou123 <hanyilou123@gmail.com>
2016-08-07 16:47:54 -0500

    [315. Count of Smaller Numbers After Self][Time Limit Exceeded]committed by LeetCode Extension

--- 315-Count-of-Smaller-Numbers-After-Self/solution.java
+++ 315-Count-of-Smaller-Numbers-After-Self/solution.java
@@ -1,13 +1,13 @@
 public class Solution {
     public List<Integer> countSmaller(int[] nums) {
         List<Integer> result = new ArrayList<Integer>();
-        if(nums==null || nums.length()==0)
+        if(nums==null || nums.length==0)
             return result;
         int count=0;
-        for(int i=0; i<nums.length-1; i++)
+        for(int i=0; i<nums.length; i++)
         {
             count=0;
-            for(int j=i+1; j<nums.length-1; j++)
+            for(int j=i+1; j<nums.length; j++)
             {
                 if(nums[j]<nums[i])
                     count++;

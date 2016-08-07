c7ba15d87c7f4517c114c45ceec6865050a2bd16
hanyilou123 <hanyilou123@gmail.com>
2016-08-07 17:37:26 -0500

    [315. Count of Smaller Numbers After Self][Compile Error]committed by LeetCode Extension

--- 315-Count-of-Smaller-Numbers-After-Self/solution.java
+++ 315-Count-of-Smaller-Numbers-After-Self/solution.java
@@ -1,19 +1,35 @@
 public class Solution {
     public List<Integer> countSmaller(int[] nums) {
-        List<Integer> result = new ArrayList<Integer>();
+        List<Integer> sorted = new ArrayList<Integer>();
         if(nums==null || nums.length==0)
             return result;
-        int count=0;
-        for(int i=0; i<nums.length; i++)
+        int len = nums.length;
+        int[] ans = new int[len];
+        for(int i=nums.length-1; i>=0; i--)
         {
-            count=0;
-            for(int j=i+1; j<nums.length; j++)
-            {
-                if(nums[j]<nums[i])
-                    count++;
-            }
-            result.add(count);
+            int index = findIndex(sorted, nums[i]);
+            ans[i] = index;
         }
-        return result;
+    }
+    public int findIndex(List<Integer> sorted, int target)
+    {
+        if(sorted.size==0)
+            return 0;
+        int start=0, end=sorted.size()-1;
+        if(sorted.get(end)<target)
+            return end+1;
+        if(sorted.get(start)>target)
+            return 0;
+        while(start+1<end)
+        {
+            int mid = (start+end)/2;
+            if(sorted.get(mid)<target)
+                start=mid+1;
+            else
+                end =mid;
+        }
+        if(sorted.get(start)>=target)
+            return start;
+        return end;
     }
 }

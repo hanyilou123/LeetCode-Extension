f5f9c5f7da62ce3d3883f1e5a5c23f7fe01d0a82
hanyilou123 <hanyilou123@gmail.com>
2016-08-08 17:29:01 -0500

    [327. Count of Range Sum][Compile Error]committed by LeetCode Extension

---
+++ 327-Count-of-Range-Sum/solution.java
@@ -0,0 +1,33 @@
+public class Solution {
+    public int countRangeSum(int[] nums, int lower, int upper) {
+        int n = nums.length();
+        long[] sums = new long[n+1];
+        for(int i=0; i<nums.length; i++)
+        {
+            sums[i+1] = sums[i]+nums[i];
+        }
+        return countWhileMerge(sums, 0, n+1, lower, upper);
+    }
+    public int countWhileMerge(long[] sums, int start, int end, int lower, upper)
+    {
+        if(end-start<=1)
+            return 0;
+        int mid = (start+end)/2;
+        int count = countWhileMerge(sums, start, mid, lower,upper) + countWhileMerge(sums, mid, end, lower, upper);
+        int j=mid, k=mid, t=mid;
+        long[] cache = new long[end-start];
+        for(int i=start, r=0; i<mid; i++, r++)
+        {
+            while(k<end && sums[k]-sums[i]<lower)
+                k++;
+            while(j<end && sums[j]-sum[i]<=upper)
+                j++;
+            while(t<end && sums[t]<sums[i])
+                cache[r++]=sums[t++];
+            cache[r]=sums[i];
+            count += j-k;
+        }
+        System.arraycopy(cache, 0, sums, start, t-start);
+        return count;
+    }
+}

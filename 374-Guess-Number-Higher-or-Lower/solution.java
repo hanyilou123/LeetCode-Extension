86d744b3289dea4e41151751a8b894a3b6188744
hanyilou123 <hanyilou123@gmail.com>
2016-08-09 17:31:47 -0500

    [374. Guess Number Higher or Lower][Accepted]committed by LeetCode Extension

--- 374-Guess-Number-Higher-or-Lower/solution.java
+++ 374-Guess-Number-Higher-or-Lower/solution.java
@@ -12,9 +12,10 @@ public class Solution extends GuessGame {
             if(guess(mid)==0)
                 return mid;
             else if(guess(mid)==-1)
-                start = mid+1;
-            else if(guess(mid)==1)
                 end = mid;
+            else if(guess(mid)==1)
+                start = mid+1;
         }
+        return start;
     }
 }

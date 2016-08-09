c18e9789ce793d7a908782609211584c6d598fea
hanyilou123 <hanyilou123@gmail.com>
2016-08-09 17:25:05 -0500

    [374. Guess Number Higher or Lower][Compile Error]committed by LeetCode Extension

--- 374-Guess-Number-Higher-or-Lower/solution.java
+++ 374-Guess-Number-Higher-or-Lower/solution.java
@@ -11,11 +11,10 @@ public class Solution extends GuessGame {
             int mid = start+(end-start)/2;
             if(guess(mid)==0)
                 return mid;
-            else if(guess(mid)=-1)
+            else if(guess(mid)==-1)
                 start = mid+1;
             else if(guess(mid)==1)
                 end = mid;
         }
-        return start;
     }
 }

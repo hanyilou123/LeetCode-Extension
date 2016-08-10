c7674bae3ee6c90248428dfb1333663d7f1353ae
hanyilou123 <hanyilou123@gmail.com>
2016-08-10 15:09:31 -0500

    [275. H-Index II][Time Limit Exceeded]committed by LeetCode Extension

--- 275-H-Index-II/solution.java
+++ 275-H-Index-II/solution.java
@@ -3,17 +3,17 @@ public class Solution {
         if(citations==null || citations.length==0)
             return 0;
         int len=citations.length;
-        int start=0, end =len-1;
-        while(start<end)
+        int start=0, end =len;
+        while(start<=end)
         {
             int mid = start+(end-start)/2;
-            if(len-1-mid=citations[mid])
-                return len-1-mid;
-            else if(len-1-mid<citations[mid])
+            if(len-mid==citations[mid])
+                return len-mid;
+            else if(len-mid<citations[mid])
                 start=mid+1;
             else
                 end = mid;
         }
-        return len-1-start;
+        return len-start;
     }
 }

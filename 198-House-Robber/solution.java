fd57076ae48488c35364813ac7082d27349d97eb
hanyilou123 <hanyilou123@gmail.com>
2016-08-11 15:47:52 -0500

    [198. House Robber][Compile Error]committed by LeetCode Extension

---
+++ 198-House-Robber/solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int rob(int[] nums) {
+        if(nums==0)
+            return 0;
+        if(nums.length==1)
+            return nums[0];
+        if(nums.length==2)
+            return Math.max(nums[0], nums[1]);
+        nums[1]=Math.max(nums[0], nums[1]);
+        for(int i=2; i<nums.length; i++)
+        {
+            nums[i]=Math.max(nums[i]+nums[i-2], nums[i]);
+        }
+        return nums[nums.length-1];
+    }
+}

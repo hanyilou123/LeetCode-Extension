9d47c7526ae9cc2b81b248c18e867de7be4be525
hanyilou123 <hanyilou123@gmail.com>
2016-08-11 15:34:41 -0500

    [174. Dungeon Game][Accepted]committed by LeetCode Extension

--- 174-Dungeon-Game/solution.java
+++ 174-Dungeon-Game/solution.java
@@ -3,21 +3,21 @@ public class Solution {
         if(dungeon==null || dungeon.length==0 || dungeon[0].length==0)
             return 0;
         int m = dungeon.length, n=dungeon[0].length;
-        int[][] health = new int[m][n];
-        health[m-1][n-1] = Math.max(1-dungeon[m-1][n-1], 1);
+        // int[][] health = new int[m][n];
+        dungeon[m-1][n-1] = Math.max(1-dungeon[m-1][n-1], 1);
         for(int i=m-2; i>=0; i--)
-            health[i][n-1] = Math.max(health[i+1][n-1]-dungeon[i][n-1], 1);
+            dungeon[i][n-1] = Math.max(dungeon[i+1][n-1]-dungeon[i][n-1], 1);
         for(int i=n-2; i>=0; i--)
-            health[m-1][i] = Math.max(health[m-1][i+1]-dungeon[m-1][i], 1);
+            dungeon[m-1][i] = Math.max(dungeon[m-1][i+1]-dungeon[m-1][i], 1);
         for(int i=m-2; i>=0; i--)
         {
             for(int j=n-2; j>=0; j--)
             {
-                int right = Math.max(health[i][j+1]-dungeon[i][j], 1);
-                int down = Math.max(health[i+1][j]-dungeon[i][j], 1);
-                health[i][j] = Math.min(right, down);
+                int right = Math.max(dungeon[i][j+1]-dungeon[i][j], 1);
+                int down = Math.max(dungeon[i+1][j]-dungeon[i][j], 1);
+                dungeon[i][j] = Math.min(right, down);
             }
         }
-        return health[0][0];
+        return dungeon[0][0];
     }
 }

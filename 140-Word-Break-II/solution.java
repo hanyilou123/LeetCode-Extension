a4cb85cf1831aa7cd5b8b7dafc732dad68c5a819
hanyilou123 <hanyilou123@gmail.com>
2016-08-12 22:00:27 -0500

    [140. Word Break II][Compile Error]committed by LeetCode Extension

---
+++ 140-Word-Break-II/solution.java
@@ -0,0 +1,31 @@
+public class Solution {
+    public List<String> wordBreak(String s, Set<String> wordDict) {
+        Map<String, List<String>> map = new HashMap<String, List<String>>();
+        return breakProcess();
+    }
+    public List<String> breakProcess(String s, Set<String> wordDict, Map<String, List<String>> map)
+    {
+        if(map.contains(s))
+            return map.get(s);
+        List<String> result = new ArrayList<String>();
+        int n = s.length();
+        if(s==null || n<=0)
+            return result;
+        for(int i=1; i<=n; i++)
+        {
+            String prefix = s.substring(0, i);
+            if(wordDict.contains(prefix))
+            {
+                String subfix = s.substring(i);
+                List<String> temp = breakProcess(subfix, wordDict, map);
+                for(String str:temp)
+                {
+                    str = prefix + " " + str;
+                    result.add(str);
+                }
+            }
+        }
+        map.add(s, result);
+        return result;
+    }
+}

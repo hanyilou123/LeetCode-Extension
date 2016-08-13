cbf6d512295e066e3bd9660ae2b1d1dc087022b9
hanyilou123 <hanyilou123@gmail.com>
2016-08-13 17:59:04 -0500

    [51. N-Queens][Compile Error]committed by LeetCode Extension

--- 51-N-Queens/solution.java
+++ 51-N-Queens/solution.java
@@ -16,7 +16,7 @@ public class Solution {
             for(int i=0; i<n; i++)
             {
                 StringBuilder row = new StringBuilder();
-                for(int j=0; j<n)
+                for(int j=0; j<n; j++)
                 {
                     if(j==state[i])
                         row.append(".");

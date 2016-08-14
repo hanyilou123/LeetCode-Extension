bbc9ca0866e49dc0ccc18a7106ee870ac2cdf509
hanyilou123 <hanyilou123@gmail.com>
2016-08-14 15:07:28 -0500

    [150. Evaluate Reverse Polish Notation][Accepted]committed by LeetCode Extension

--- 150-Evaluate-Reverse-Polish-Notation/solution.java
+++ 150-Evaluate-Reverse-Polish-Notation/solution.java
@@ -17,9 +17,9 @@ public class Solution {
             }
             else if(tokens[i].equals("/"))
             {
-                // int n1=stack.pop(), n2=stack.pop();
-                // stack.push(n2/n1);   
-                stack.push(1/stack.pop()*stack.pop());  //使用这种方法会出现／0 的情况
+                int n1=stack.pop(), n2=stack.pop();
+                stack.push(n2/n1);   
+                //stack.push(1/stack.pop()*stack.pop());  //使用这种方法会出现／0 的情况
             }
             else
             {

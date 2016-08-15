e8ffecba0fad49299b95173eb15f251befa74eda
hanyilou123 <hanyilou123@gmail.com>
2016-08-14 20:59:37 -0500

    [239. Sliding Window Maximum][Compile Error]committed by LeetCode Extension

---
+++ 239-Sliding-Window-Maximum/solution.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if(nums==null || nums.length==0 || k<=0)
+            return new int[0];
+        Deque<Integer> dq = new ArrayDeque<Integer>();
+        int len = nums.length;
+        int[] res = new int[len-k+1];
+        int i=0;
+        for(int i=0; i<nums.length; i++)
+        {
+            while(!dq.isEmpty() && dq.peek()<i-k+1)
+            {
+                dq.pollFirst();
+            }
+            while(!dq.isEmpty() && nums[dq.peekLast()]<nums[i])
+            {
+                dq.pollLast();
+            }
+            if(i>=k-1)
+            {
+                res[i++]=dq.peekFirst();
+            }
+        }
+        return res;
+    }
+}

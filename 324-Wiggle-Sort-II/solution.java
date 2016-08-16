e97cc96f200a88fd592026f9efbf6c4d143e0432
hanyilou123 <hanyilou123@gmail.com>
2016-08-16 18:18:54 -0500

    [324. Wiggle Sort II][Runtime Error]committed by LeetCode Extension

---
+++ 324-Wiggle-Sort-II/solution.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public void wiggleSort(int[] nums) {
+        if(nums==null || nums.length==0)
+            return;
+        Arrays.sort(nums);
+        int small=(nums.length-1)/2, big=nums.length;
+        int[] temp = new int[nums.length];
+        for(int i=0; i<nums.length; i++)
+        {
+            temp[i] = (i&1)==0?nums[small--]:nums[big--];
+        }
+        for(int i=0; i<nums.length; i++)
+        {
+            nums[i] = temp[i];
+        }
+    }
+}

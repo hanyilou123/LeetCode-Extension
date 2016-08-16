9a42d9e73c1cb1f4ec49192e84f7c6c11f3cd501
hanyilou123 <hanyilou123@gmail.com>
2016-08-16 17:46:01 -0500

    [330. Patching Array][Accepted]committed by LeetCode Extension

--- 330-Patching-Array/solution.java
+++ 330-Patching-Array/solution.java
@@ -1,6 +1,7 @@
 public class Solution {
     public int minPatches(int[] nums, int n) {
-        long known=1, count=0, i=0;
+        long known=1;
+        int count=0, i=0;
         while(known<=n)
         {
             if(i<nums.length && known>=nums[i])

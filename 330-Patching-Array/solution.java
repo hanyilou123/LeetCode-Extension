4e2b56650ca56c1a638880dc3237e1fa0e2afba0
hanyilou123 <hanyilou123@gmail.com>
2016-08-16 17:45:44 -0500

    [330. Patching Array][Compile Error]committed by LeetCode Extension

--- 330-Patching-Array/solution.java
+++ 330-Patching-Array/solution.java
@@ -1,6 +1,6 @@
 public class Solution {
     public int minPatches(int[] nums, int n) {
-        int known=1, count=0, i=0;
+        long known=1, count=0, i=0;
         while(known<=n)
         {
             if(i<nums.length && known>=nums[i])

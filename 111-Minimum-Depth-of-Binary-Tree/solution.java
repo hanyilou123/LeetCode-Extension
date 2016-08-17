7a689955e2e7ed89f3a02e28c8e51370fbd8683c
hanyilou123 <hanyilou123@gmail.com>
2016-08-17 16:08:00 -0500

    [111. Minimum Depth of Binary Tree][Accepted]committed by LeetCode Extension

--- 111-Minimum-Depth-of-Binary-Tree/solution.java
+++ 111-Minimum-Depth-of-Binary-Tree/solution.java
@@ -11,9 +11,14 @@ public class Solution {
     public int minDepth(TreeNode root) {
         if(root==null)
             return 0;
-        if(root.left!=null && root.right!=null)
-            return Math.min(minDepth(root.left), minDepth(root.right))+1;
-        else
-            return Math.max(minDepth(root.left), minDepth(root.right))+1;
+        return process(root);
+    }
+    public int process(TreeNode root)
+    {
+        if(root==null)
+            return Integer.MAX_VALUE;
+        if(root.left==null && root.right==null)
+            return 1;
+        return Math.min(process(root.left), process(root.right))+1;
     }
 }

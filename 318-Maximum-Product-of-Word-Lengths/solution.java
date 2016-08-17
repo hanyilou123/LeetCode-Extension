a485b615727d61762aaa6a5f80542277daa13a3f
hanyilou123 <hanyilou123@gmail.com>
2016-08-17 02:40:00 -0500

    [318. Maximum Product of Word Lengths][Accepted]committed by LeetCode Extension

--- 318-Maximum-Product-of-Word-Lengths/solution.java
+++ 318-Maximum-Product-of-Word-Lengths/solution.java
@@ -1,16 +1,16 @@
 public class Solution {
-    public class compLen implements Comparator<String>{
-        public int compare(String a, String b)
-        {
-            return b.length()-a.length();
-        }
-    }
+    // public class compLen implements Comparator<String>{
+    //     public int compare(String a, String b)
+    //     {
+    //         return b.length()-a.length();
+    //     }
+    // }
     public int maxProduct(String[] words) {
         if(words==null || words.length==0)
             return 0;
         int len = words.length;
         int[] bitmap = new int[len];
-        Arrays.sort(words, new compLen());
+        // Arrays.sort(words, new compLen());
         for(int i=0; i<len; i++)
         {
             for(char c: words[i].toCharArray())
@@ -21,8 +21,8 @@ public class Solution {
         int max=0;
         for(int i=0; i<len; i++)
         {
-            if(words[i].length()*words[i].length()<max)
-                break;
+            // if(words[i].length()*words[i].length()<max)
+            //     break;
             for(int j=i+1; j<len; j++)
             {
                 if((bitmap[i]&bitmap[j])==0)

b44d79900cd306c302bc39141f10d73d4290d0e0
hanyilou123 <hanyilou123@gmail.com>
2016-08-17 15:55:52 -0500

    [111. Minimum Depth of Binary Tree][Compile Error]committed by LeetCode Extension

--- 111-Minimum-Depth-of-Binary-Tree/solution.java
+++ 111-Minimum-Depth-of-Binary-Tree/solution.java
@@ -11,6 +11,7 @@ public class Solution {
     int minDep = Integer.MAX_VALUE;
     public int minDepth(TreeNode root) {
         process(root, 0);
+        return minDep;
     }
     public void process(TreeNode root, int depth)
     {
@@ -19,7 +20,11 @@ public class Solution {
             minDep = Math.min(depth, minDep);
             return;
         }
-        process(root.left, depth+1);
-        process(root.right, depth+1);
+        if(root.left==null && root.right==null)
+            return 1;
+        if(root.left!=null)
+            process(root.left, depth+1);
+        if(root.right!=null)
+            process(root.right, depth+1);
     }
 }

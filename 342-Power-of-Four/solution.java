067d5ec0c8c6041a8172d6208ad925729e4207e1
hanyilou123 <hanyilou123@gmail.com>
2016-08-16 23:54:12 -0500

    [342. Power of Four][Compile Error]committed by LeetCode Extension

--- 342-Power-of-Four/solution.java
+++ 342-Power-of-Four/solution.java
@@ -1,5 +1,5 @@
 public class Solution {
     public boolean isPowerOfFour(int num) {
-        return num>0 && (int)(Math.log(num)/Math.log(4))-(Math.log(num)/Math.log(4))==0;
+        return num>0 && !num(num-1) && num&0x55555555=num;
     }
 }

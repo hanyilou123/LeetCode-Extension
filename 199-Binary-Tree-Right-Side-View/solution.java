62d9bfa5b8ebd8dc63fb0e0e8de70d5d1b0d5e8c
hanyilou123 <hanyilou123@gmail.com>
2016-08-17 16:44:42 -0500

    [199. Binary Tree Right Side View][Compile Error]committed by LeetCode Extension

---
+++ 199-Binary-Tree-Right-Side-View/solution.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        if(root==null)
+            return res;
+        viewProcess(root, res, 0);
+        return res;
+    }
+    public void viewProcess(TreeNode root, List<Integer> res, int height)
+    {
+        if(root==null)
+            return;
+        if(height==root.size())
+        {
+            res.add(root.val);
+        }
+        viewProcess(root.right, res, height+1);
+        viewProcess(root.left, res, height+1);
+    }
+}

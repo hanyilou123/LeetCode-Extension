1078c9a245a113f316acfcf155db8259214b8394
hanyilou123 <hanyilou123@gmail.com>
2016-08-18 16:44:30 -0500

    [210. Course Schedule II][Compile Error]committed by LeetCode Extension

--- 210-Course-Schedule-II/solution.java
+++ 210-Course-Schedule-II/solution.java
@@ -1,41 +1,37 @@
 public class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        int[][] matrix = new int[numCourses][numCourses];
-        int[] indegree = new int[numCourses];
+        int[] outdegree = new int[numCourses];
         int[] result = new int[numCourses];
         for(int i=0; i<prerequisites.length; i++)
         {
-            int pre = prerequisites[i][1];
-            int after = prerequisites[i][0];
-            if(matrix[pre][after]==0)
-                indegree[after]++;
-            matrix[pre][after]=1;
+            outdegree[prerequisites[i][1]]++;
         }
         Queue<Integer> que = new LinkedList<Integer>();
         for(int i=0; i<numCourses; i++)
         {
-            if(indegree[i]==0)
+            if(outdegree[i]==0)
                 que.offer(i);
         }
-        int count=0;
+        int count=numCourses-1;
         while(!que.isEmpty())
         {
             int cur = que.poll();
-            result[count++]=cur;
-            for(int i=0; i<numCourses; i++)
+            result[count--]=cur;
+            for(int i=0; i<prerequisites.length; i++)
             {
-                if(matrix[cur][i]!=0)
+                int l = prerequisites[i][1];
+                if(matrix[i][0]==cur)
                 {
-                    if(--indegree[i]==0)
+                    if(--outdegree[l]==0)
                     {
                         que.offer(i);
                     }
                 }
             }
         }
-        if(count==numCourses)
+        if(count!=-1)
             return result;
         else
-            return new int[];
+            return new int[0];
     }
 }

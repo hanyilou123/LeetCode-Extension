da416653c5643b21f9d9428f57169b17aa4a87b8
hanyilou123 <hanyilou123@gmail.com>
2016-08-19 18:24:34 -0500

    [329. Longest Increasing Path in a Matrix][Compile Error]committed by LeetCode Extension

--- 329-Longest-Increasing-Path-in-a-Matrix/solution.java
+++ 329-Longest-Increasing-Path-in-a-Matrix/solution.java
@@ -1,28 +1,36 @@
 public class Solution {
     int[] dx = {-1, 0, 1, 0};
     int[] dy = {0, 1, 0, -1};
-    int maxValue = 1;
+    
     public int longestIncreasingPath(int[][] matrix) {
         if(matrix==null || matrix.length==0 || matrix[0].length==0)
             return 0;
+        int[][] cache = new int[matrix.length][matrix.length];
+        int maxValue = 1;
         for(int i=0; i<matrix.length; i++)
         {
             for(int j=0; j<matrix[i].length; j++)
             {
-                dfs(matrix, i, j, 1);
+                int length = dfs(matrix, i, j, 1, cache);
+                maxValue = Math.max(length, maxValue);
             }
         }
         return maxValue;
     }
-    public void dfs(int[][] matrix, int i, int j, int count)
+    public int dfs(int[][] matrix, int i, int j, int count, int[][] cache)
     {
+        if(cache[i][j]>0)
+            return cache[i][j];
+        int max=1;
         for(int k=0; k<dx.length; k++)
         {
             if(i+dx[k]>=0 && i+dx[k]<matrix.length && j+dy[k]>=0 && j+dy[k]<matrix[i].length && matrix[i+dx[k]][j+dy[k]]>matrix[i][j])
             {
-                maxValue=Math.max(maxValue, count+1);
-                dfs(matrix, i+dx[k], j+dy[k], count+1);
+                int len = 1+dfs(matrix, i+dx[k], j+dy[k], count+1);
+                max=Math.max(max, len);
             }
         }
+        cache[i][j]=max;
+        return max;
     }
 }

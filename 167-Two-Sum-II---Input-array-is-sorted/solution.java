8342492e03947e42baa1f00df304d0f16ab2bad6
hanyilou123 <hanyilou123@gmail.com>
2016-08-20 18:39:09 -0500

    [167. Two Sum II - Input array is sorted][Compile Error]committed by LeetCode Extension

---
+++ 167-Two-Sum-II---Input-array-is-sorted/solution.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        if(numbers==null || numbers.length==0)
+            return new int[2];
+        int idx1=0, idx2=numbers.length-1;
+        int[] res = new int[];
+        while(idx1<idx2)
+        {
+            if(numbers[idx1]+numbers[idx2]==target)
+                break;
+            else if(numbers[idx1]+numbers[idx2]>target)
+                idx2--;
+            else
+                idx1++;
+        }
+        res[0] = idx1;
+        res[1] = idx2;
+        return res;
+    }
+}

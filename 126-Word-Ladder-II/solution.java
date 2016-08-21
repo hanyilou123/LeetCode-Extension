881504df88a656da6907007164b970b60ff123cc
hanyilou123 <hanyilou123@gmail.com>
2016-08-21 17:38:33 -0500

    [126. Word Ladder II][Compile Error]committed by LeetCode Extension

--- 126-Word-Ladder-II/solution.java
+++ 126-Word-Ladder-II/solution.java
@@ -3,16 +3,23 @@ public class Solution {
     public List<List<String>> findLadders(String beginWord, String endWord, Set<String> wordList) {
         List<List<String>> result = new ArrayList<List<String>>();
         List<String> path = new ArrayList<String>();
-        bfs(beginWord, endWord, wordList);
-        dfs(endWord, beginWord, wordList, path, result);
+        Map<String, List<String>> map = new HashMap<String, List<String>>();
+        wordList.add(beginWord);
+        wordList.add(endWord);
+        bfs(beginWord, endWord, wordList, map);
+        dfs(endWord, beginWord, wordList, path, result, map);
         return result;
     }
     
-    public void bfs(String start, String end, Set<String> dict)
+    public void bfs(String start, String end, Set<String> dict, Map<String, List<String>> map)
     {
         Queue<String> que = new LinkedList<String>();
         que.offer(start);
         pathDist.put(start, 0);
+        for(String s: dict)
+        {
+            map.put(s, new ArrayList<String>())
+        }
         while(!que.isEmpty())
         {
             String cur = que.poll();
@@ -27,9 +34,10 @@ public class Solution {
                         continue;
                     curArray[i]=ch;
                     String newCur = new String(curArray);
-                    if(dict.contains(newCur) || newCur.equals(end))
+                    if(dict.contains(newCur))
                     {
-                        if(pathDist.get(newCur)==null)    //pathDist.containsKey(newCur);
+                        map.get(newCur).add(cur);
+                        if(pathDist.get(newCur)==null)    //!pathDist.containsKey(newCur);
                         {
                             int depth = pathDist.get(cur);
                             pathDist.put(newCur, depth+1);
@@ -41,33 +49,22 @@ public class Solution {
         }
     }
     
-    public void dfs(String start, String end, Set<String> dict, List<String> path, List<List<String>> result)
+    public void dfs(String start, String end, Set<String> dict, List<String> path, List<List<String>> result, Map<String, List<String>> map)
     {
+        path.add(start);
         if(start.equals(end))
         {
-            path.add(start);
             Collections.reverse(path);
-            result.add(path);
-            return;
+            result.add(new ArrayList<String>(path));
+            Collections.reverse(path);
         }
-        if(pathDist.get(start)==null)
-            return;
-        path.add(start);
-        int nextDist = pathDist.get(start)-1;
-        for(int i=0; i<start.length(); i++)
-        {
-            char[] curArray = cur.toCharArray();
-            for(char ch='a'; ch<='z'; ch++)
+        else{
+            for(String next: map.get(start))
             {
-                if(curArray[i]==ch)
-                    continue;
-                curArray[i]=ch;
-                String newCur = new String(curArray);
-                if(pathDist.get(newCur)!=null && pathDist.get(newCur)==nextDist)
-                {
-                    dfs(newCur, end, dict, path, result);
-                }
+                if(pathDist.containsKey(next) && pathDist.get(next)+1==pathDist.get(start))
+                    dfs(next, end, dict, path, result, map);
             }
         }
     }
+    path.remove(path.size()-1);
 }

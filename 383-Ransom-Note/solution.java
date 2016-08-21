d8571155a77c222c2380648ea78f691f64d21409
hanyilou123 <hanyilou123@gmail.com>
2016-08-21 17:54:55 -0500

    [383. Ransom Note][Accepted]committed by LeetCode Extension

--- 383-Ransom-Note/solution.java
+++ 383-Ransom-Note/solution.java
@@ -1,14 +1,14 @@
 public class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
         int[] letters = new int[26];
-        for(int i=0; i<ransomNote.length(); i++)
-        {
-            int idx1 = ransomNote.charAt(i)-'a';
-            letters[idx]++;
-        }
         for(int i=0; i<magazine.length(); i++)
         {
-            int idx2 = magazine.charAt(i);
+            int idx1 = magazine.charAt(i)-'a';
+            letters[idx1]++;
+        }
+        for(int i=0; i<ransomNote.length(); i++)
+        {
+            int idx2 = ransomNote.charAt(i)-'a';
             if(--letters[idx2]<0)
                 return false;
         }

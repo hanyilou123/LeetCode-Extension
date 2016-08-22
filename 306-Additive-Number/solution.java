d5c66acacea6faf2698070ed1791c75e79afcd44
hanyilou123 <hanyilou123@gmail.com>
2016-08-21 23:12:05 -0500

    [306. Additive Number][Compile Error]committed by LeetCode Extension

---
+++ 306-Additive-Number/solution.java
@@ -0,0 +1,32 @@
+public class Solution {
+    public boolean isAdditiveNumber(String num) {
+        if(num==null || num.length()<2)
+            return false;
+        int len = num.length();
+        for(int i=1; i<=len/2; i++)
+        {
+            for(int j=1; Math.max(i, j)<=len-i-j; j++)
+            {
+                if(isValid(i, j, num))
+                    return true;
+            }
+        }
+    }
+    public boolean isValid(int i, int j, String num)
+    {
+        if(num.charAt(0)=='0' || num.charAt(i)=='0')
+            return false;
+        Long x1 = Long.parseLong(num.substring(0, i));
+        Long x2 = Long.parseLong(num.substring(i, i+j));
+        String sum = "";
+        for(int k=i+j; k<num.length(); k+=sum.length())
+        {
+            x2 = x1+x2;
+            x1 = x2-x1;
+            sum = String.valueOf(x2);
+            if(!num.startsWith(sum, k))
+                return false;
+        }
+        return true;
+    }
+}

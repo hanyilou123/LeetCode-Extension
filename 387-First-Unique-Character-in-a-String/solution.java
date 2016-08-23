a198f845c81f0ea40c4c15e268ebc7f28c131d7a
hanyilou123 <hanyilou123@gmail.com>
2016-08-22 21:42:22 -0500

    [387. First Unique Character in a String][Accepted]committed by LeetCode Extension

--- 387-First-Unique-Character-in-a-String/solution.java
+++ 387-First-Unique-Character-in-a-String/solution.java
@@ -3,13 +3,14 @@ public class Solution {
         if(s==null || s.length()==0)
             return -1;
         int[] letters = new int[26];
-        for(int i=0; i<s.length(); i++)
+        char[] sToArray = s.toCharArray();
+        for(int i=0; i<sToArray.length; i++)
         {
-            letters[s.charAt(i)-'a']++;
+            letters[sToArray[i]-'a']++;
         }
-        for(int i=0; i<s.length(); i++)
+        for(int i=0; i<sToArray.length; i++)
         {
-            if(letters[s.charAt(i)-'a']==1)
+            if(letters[sToArray[i]-'a']==1)
                 return i;
         }
         return -1;

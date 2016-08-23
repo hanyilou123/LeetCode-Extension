7c37385ecd99f9d40f876630d6ea1ef177bbc708
hanyilou123 <hanyilou123@gmail.com>
2016-08-23 02:32:51 -0500

    [386. Lexicographical Numbers][Accepted]committed by LeetCode Extension

--- 386-Lexicographical-Numbers/solution.java
+++ 386-Lexicographical-Numbers/solution.java
@@ -1,18 +1,26 @@
 public class Solution {
-    public class lexiComp implements Comparator<Integer>{
-        public int compare(Integer a, Integer b){
-            String aa = String.valueOf(a);
-            String bb = String.valueOf(b);
-            return aa.compareTo(bb);
-        }
-    }
     public List<Integer> lexicalOrder(int n) {
         List<Integer> result = new ArrayList<Integer>();
+        int cur = 1;
         for(int i=1; i<=n; i++)
         {
-            result.add(i);
+            result.add(cur);
+            if(cur*10<=n)
+            {
+                cur *= 10;
+            }
+            else if(cur%10!=9 && cur+1<=n)
+            {
+                cur++;
+            }
+            else{
+                while((cur/10) %10==9)
+                {
+                    cur /= 10;
+                }
+                cur = cur/10+1;
+            }
         }
-        Collections.sort(result, new lexiComp());
         return result;
     }
 }
